050c2b3ceabca845793cde63c89bd9b2d7825d6d
McCabe, Robert J <Robert.McCabe@rockwellcollins.com>
2017-07-22 01:14:12 -0500

    Added repeat last macro with Enter

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-files=(mygitconfig myzshrc dircolors myvimrc_vundle_plugins)
+files=(mygitconfig myzshrc dircolors myvimrc_vundle_plugins myvimrc)
 
 for f in ${files[@]}
 do
