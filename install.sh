48910a7cfc01948282f0b6c21481b81f55002188
McCabe, Robert J <robert.mccabe@rockwellcollins.com>
2019-10-02 22:42:51 -0500

    Don't assume Linux system in install.sh
    Now works on my my Macbook

--- install.sh
+++ install.sh
@@ -14,11 +14,17 @@ if [ ! -d ~/.vim/bundle/youcompleteme ]; then
    exit 1
 fi
 
-sudo apt-get update
-sudo apt install build-essential cmake python3-dev \
-   neovim python3-neovim
+case "$(uname -s)" in
+   Linux)
+      #TODO: this only works for debian-based systems
+      sudo apt-get update
+      sudo apt install build-essential cmake python3-dev \
+         neovim python3-neovim
+      sudo apt install python3-dev python3-pip python3-setuptools
+      sudo pip3 install thefuck
+     ;;
+esac
+
 cd ~/.vim/bundle/youcompleteme
 python3 ./install.py --clang-completer
 
-sudo apt install python3-dev python3-pip python3-setuptools
-sudo pip3 install thefuck
