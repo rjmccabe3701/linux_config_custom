318e2c6c224c9e8064f2477ed798ebed4360542f
McCabe, Robert J <robert.mccabe@cubic.com>
2022-01-04 18:46:18 -0800

    Don't require sudo

--- custom_install.sh
+++ custom_install.sh
@@ -4,35 +4,35 @@ echo "CUSTOM INSTALL"
 rm -rf ~/user_install
 mkdir -p ~/user_install
 
-
-case "$(uname -s)" in
-   Linux)
-      if [ -f /etc/fedora-release ]
-      then
-         sudo dnf install -y \
-            cmake \
-            g++ \
-            make \
-            neovim \
-            python3-devel \
-            python3-neovim
-
-         sudo dnf install -y python3-pip python3-setuptools
-      else
-         #Assumes debian
-         sudo apt-get update
-         sudo apt install -y build-essential cmake python3-dev \
-            neovim python3-neovim
-         sudo apt install -y python3-dev python3-pip python3-setuptools
-         sudo apt install -y clang-format
-      fi
-      ;;
-esac
-
 curl https://raw.githubusercontent.com/llvm-mirror/clang/master/tools/clang-format/clang-format.py > ~/.vim/clang-format.py
 
-cd ~/.vim/plugged/youcompleteme
-python3 ./install.py --clang-completer
-
-sudo python3 -m pip install pynvim autopep8 --upgrade
+# case "$(uname -s)" in
+#    Linux)
+#       if [ -f /etc/fedora-release ]
+#       then
+#          sudo dnf install -y \
+#             cmake \
+#             g++ \
+#             make \
+#             neovim \
+#             python3-devel \
+#             python3-neovim
+#
+#          sudo dnf install -y python3-pip python3-setuptools
+#       else
+#          #Assumes debian
+#          sudo apt-get update
+#          sudo apt install -y build-essential cmake python3-dev \
+#             neovim python3-neovim
+#          sudo apt install -y python3-dev python3-pip python3-setuptools
+#          sudo apt install -y clang-format
+#       fi
+#       ;;
+# esac
+#
+#
+# #cd ~/.vim/plugged/youcompleteme
+# #python3 ./install.py --clang-completer
+#
+# sudo python3 -m pip install pynvim autopep8 --upgrade
 
