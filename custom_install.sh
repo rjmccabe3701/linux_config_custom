ea940aeed4013e31d84cf4aae70d759e93879e4f
McCabe, Robert J <robert.mccabe@collins.com>
2024-05-29 19:07:21 +0000

    Added nvim override to disable inlay hints

--- custom_install.sh
+++ custom_install.sh
@@ -8,6 +8,8 @@ mkdir -p ~/.git
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 
+ln -sf ${DIR}/rob.lua ~/.config/nvim/lua/plugins/rob.lua
+
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
---
+++ rob.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		-- https://github.com/LazyVim/LazyVim/discussions/3191
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+		},
+	},
+}
