bff7615e11b26c59f80d01dfd66d7d9d80e5d733
McCabe, Robert J <robert.mccabe@collins.com>
2024-07-03 18:24:42 +0000

    Remove s/S mapping for the flash plugin
    This conflicts with the substitude command.
    Re-mapped to <leader>S

--- rob_plugins.lua
+++ rob_plugins.lua
@@ -1,13 +1,29 @@
 return {
-	{
-		-- https://github.com/LazyVim/LazyVim/discussions/3191
-		"neovim/nvim-lspconfig",
-		opts = {
-			inlay_hints = { enabled = false },
-		},
-	},
-	{
-		"echasnovski/mini.pairs",
-		enabled = false,
-	},
+  {
+    -- https://github.com/LazyVim/LazyVim/discussions/3191
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    keys = {
+      -- disable the default flash keymap
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+      {
+        "<leader>S",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+    },
+  },
 }
