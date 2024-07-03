d8c428b5f3e112a4db28e36dca32598bfe9c1f21
McCabe, Robert J <robert.mccabe@collins.com>
2024-07-03 18:56:06 +0000

    Added keymap to grep string under cursor

--- rob_plugins.lua
+++ rob_plugins.lua
@@ -1,29 +1,42 @@
 return {
-  {
-    -- https://github.com/LazyVim/LazyVim/discussions/3191
-    "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = false },
-    },
-  },
-  {
-    "echasnovski/mini.pairs",
-    enabled = false,
-  },
-  {
-    "folke/flash.nvim",
-    keys = {
-      -- disable the default flash keymap
-      { "s", mode = { "n", "x", "o" }, false },
-      { "S", mode = { "n", "x", "o" }, false },
-      {
-        "<leader>S",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-    },
-  },
+	{
+		-- https://github.com/LazyVim/LazyVim/discussions/3191
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+		},
+	},
+	{
+		"echasnovski/mini.pairs",
+		enabled = false,
+	},
+	{
+		"folke/flash.nvim",
+		keys = {
+			-- disable the default flash keymap
+			{ "s", mode = { "n", "x", "o" }, false },
+			{ "S", mode = { "n", "x", "o" }, false },
+			{
+				"<leader>S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{
+				"<C-\\>",
+				mode = { "n" },
+				function()
+					require("telescope.builtin").grep_string()
+				end,
+				desc = "Grep String Under Cursor",
+			},
+		},
+	},
 }
