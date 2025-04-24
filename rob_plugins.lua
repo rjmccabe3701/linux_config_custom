0aee0d4e5fa62b0c586ccd9c1166de45ffed5c4e
McCabe, Robert J <robert.mccabe@collins.com>
2025-04-24 22:45:23 +0000

    Fixed pylsp
    Looks like there is not a way to disable mypy thru LazyVim.
    So as workaround add a mypy.ini file at the top of your project
    with the contents:
      [mypy]
      check_untyped_defs = False
      ignore_missing_imports = True
      follow_imports = skip
      warn_unused_ignores = False
      ignore_errors = True
    This effectively disables mypy

--- rob_plugins.lua
+++ rob_plugins.lua
@@ -4,100 +4,11 @@ return {
 		opts = {
 			inlay_hints = { enabled = false },
 			servers = {
-				-- pylsp = {
-				-- settings = {
-				-- 	pylsp = {
-				-- 		-- configurationSources = {},
-				-- 		-- plugins = {},
-				-- 		plugins = {
-				-- 			pyright = { enabled = true },
-				-- 			ruff = { enabled = false },
-				-- 			autopep8 = { enabled = false },
-				-- 			black = { enabled = false },
-				-- 			yapf = { enabled = false },
-				-- 			flake8 = { enabled = false },
-				-- 			mypy = { enabled = false },
-				-- 			pyflakes = { enabled = false },
-				-- 			pycodestyle = { enabled = false },
-				-- 			jedi = { enabled = true },
-				-- 			mccabe = { enabled = false },
-				-- 			rope = { enabled = false },
-				-- 		},
-				-- 	},
-				-- },
-				--
 				pylsp = {},
 				gopls = {}, -- Basic setup for gopls
 				clangd = {}, -- Basic setup for clangd
 			},
-			setup = {
-				pylsp = function(_, opts)
-					opts.settings = vim.tbl_deep_extend("force", opts.settings or {}, {
-						pylsp = {
-							configurationSources = {},
-							plugins = {
-								pyright = { enabled = false },
-								ruff = { enabled = false },
-								autopep8 = { enabled = false },
-								black = { enabled = false },
-								yapf = { enabled = false },
-								flake8 = { enabled = false },
-								mypy = { enabled = false },
-								pyflakes = { enabled = false },
-								pycodestyle = { enabled = false },
-								jedi = { enabled = false },
-								jedi_completion = { enabled = false },
-								jedi_hover = { enabled = false },
-								jedi_references = { enabled = false },
-								jedi_signature_help = { enabled = false },
-								jedi_symbols = { enabled = false },
-								mccabe = { enabled = false },
-								rope = { enabled = false },
-							},
-						},
-					})
-					-- opts.settings = opts.settings or {}
-					-- opts.settings.pylsp = opts.settings.pylsp or {}
-					-- opts.settings.pylsp.configurationSources = {}
-					-- opts.settings.pylsp.plugins = {
-					-- 	pyright = { enabled = false },
-					-- 	ruff = { enabled = false },
-					-- 	autopep8 = { enabled = false },
-					-- 	black = { enabled = false },
-					-- 	yapf = { enabled = false },
-					-- 	flake8 = { enabled = false },
-					-- 	mypy = { enabled = false },
-					-- 	pyflakes = { enabled = false },
-					-- 	pycodestyle = { enabled = false },
-					-- 	jedi = { enabled = false },
-					-- 	jedi_completion = { enabled = false },
-					-- 	jedi_hover = { enabled = false },
-					-- 	jedi_references = { enabled = false },
-					-- 	jedi_signature_help = { enabled = false },
-					-- 	jedi_symbols = { enabled = false },
-					-- 	-- jedi = { enabled = true },
-					-- 	-- jedi_completion = { enabled = true },
-					-- 	-- jedi_hover = { enabled = true },
-					-- 	-- jedi_references = { enabled = true },
-					-- 	-- jedi_signature_help = { enabled = true },
-					-- 	-- jedi_symbols = { enabled = true },
-					-- 	mccabe = { enabled = false },
-					-- 	rope = { enabled = false },
-					-- }
-					-- -- return false
-					-- -- return true -- Indicate that we've handled the setup for pylsp
-				end,
-			},
 		},
-		-- Adding this turned off LSP entirely ...
-		-- config = function()
-		-- 	require("lspconfig").pylsp.setup({
-		-- 		on_attach = function(client, _)
-		-- 			client.server_capabilities.documentFormattingProvider = false
-		-- 			client.server_capabilities.documentRangeFormattingProvider = false
-		-- 		end,
-		-- 	})
-		-- end,
 	},
 	{
 		"echasnovski/mini.pairs",
